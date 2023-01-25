0f4d3d4c176d6f90e6e7ecda1f1da09f2a6859f1
KCzyzCTI <235768@edu.p.lodz.pl>
2023-01-25 13:07:28 +0100

    added some improvements

--- AutomeasUI/ViewModel/DashboardViewModel.cs
+++ AutomeasUI/ViewModel/DashboardViewModel.cs
@@ -4,9 +4,12 @@ using System.Collections.ObjectModel;
 using System.Globalization;
 using System.IO;
 using System.IO.Ports;
+using System.Linq;
 using System.Net.Mime;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 using AutomeasAsyncCommunication;
 using AutomeasToolkit.Basic.TitledList;
@@ -28,6 +31,9 @@ public partial class DashboardViewModel : ObservableObject
     //private readonly Mcu _mcu = new Mcu("COM10",9600);
     //private readonly Gauge _gauge = new Gauge("COM5");
     public ObservableType<bool> isStartEnabled { get; set; }= new(true);
+    public ObservableType<int> progressBarMax { get; set; } = new(1);
+    public ObservableType<int> progressBarIndex { get; set; } = new(0);
+    public ObservableType<Visibility> progressBarVisible { get; set; } = new(Visibility.Hidden);
     public ObservableType<string> Title { get; set; }
     public ObservableType<string> Subtitle { get; set; }
     public ObservableType<string> EstimatedTime { get; set; }
@@ -42,20 +48,70 @@ public partial class DashboardViewModel : ObservableObject
     public RelayCommand HaltExperimentCommand { get; set; }
     public CancellationTokenSource tokenSource;
 
+    private void AutoscaleGraph(double measuredValue, double margin){
+        if (measuredValue > YAxes[0].MaxLimit)
+        {
+            YAxes[0].MaxLimit = measuredValue + margin;
+        }
+        else if (measuredValue < YAxes[0].MinLimit)
+        {
+            YAxes[0].MinLimit = measuredValue - margin;
+        }
+    }
+
+    private string LoadMeasPath(string path)
+    {
+        string today = DateTime.Today.ToString();
+        { // parse current date
+            today = today.Split(' ')[0];
+            today = today.Replace('.', '-');
+        }
+        {
+            // check if dir exists, if not init dir
+            if (path.Last() != '\\')
+            {
+                path += "\\";
+            }
+
+            string dir = $"{path}{today}";
+            if (!Directory.Exists(dir))
+            {
+                Directory.CreateDirectory(dir);
+            }
+
+            return dir;
+        }
+    }
+
+    private string GenerateResultFileName(string path)
+    {
+        string ctime;
+        int fileCount;
+        { // get filename
+            ctime = DateTime.Now.ToString("hh-mm");
+            path = LoadMeasPath(path);
+            fileCount = (from file in Directory.EnumerateFiles(path, "*.txt", SearchOption.TopDirectoryOnly)
+                select file).Count(); 
+        }
+        string fileName = Path.Combine(path, $"results{fileCount}_time_{ctime}.txt");
+        return fileName;
+    }
     /// <summary>
     /// Execute instructions determined by the user via UI
     /// </summary>
     public void CommenceExperiment()
     {
         isStartEnabled.Value = false;
+        progressBarVisible.Value = Visibility.Visible;
+        // [DOES NOT WORK] TODO verify that cancelling token really kills process
         // TODO autoscale graph
-        // TODO sometimes launching this process fails - port is opened but nothing happens, make relaunch or sth
         // 0. import relevant settings
         Dictionary<string, object> Settings = new()
         {
             { "step", ((Combobox)ConfigBar.Collumns["TypRuchuRight"][0]).GetValue() },
             { "repeats", ((Combobox)ConfigBar.Collumns["TypRuchuLeft"][1]).GetValue() }
         };
+        progressBarMax.Value = Convert.ToUInt16(Settings["repeats"]);
         List<byte[]> exe;
         Mcu mcu = new("COM5", 9600);
         mcu.Deactivate();
@@ -75,7 +131,9 @@ public partial class DashboardViewModel : ObservableObject
             // 3. Execute
             {
                 string line;
+                string fileName = GenerateResultFileName(@"C:\Users\Kczyz\Desktop\AutomeasResults");
                 double? result;
+                
                 for (int i = 0; i < Convert.ToUInt16((string)Settings["repeats"]); i++)
                 {
                     gauge.Port.DiscardInBuffer();
@@ -84,31 +142,36 @@ public partial class DashboardViewModel : ObservableObject
 
 
                     {
+                        
                         using (FileStream fs =
-                               new FileStream(Path.Combine(@"C:\Users\Kczyz\Desktop", "measurements.txt"),
+                               new FileStream(fileName,
                                    FileMode.Append, FileAccess.Write, FileShare.None))
                         using (StreamWriter fw = new StreamWriter(fs))
                         {
                             gauge.SendSafeRequest("3");
-                            _measuredValues.RemoveAt(0);
                             result = Convert.ToDouble(gauge.GetMeasurement(Program.MeasurementType.PowerMeasDbm),
                                 CultureInfo.InvariantCulture);
+                            _measuredValues.RemoveAt(0);
                             _measuredValues.Add(new(result));
                             line = $"{result.ToString()}\t 1300nm";
                             fw.WriteLine(line);
                             gauge.SendSafeRequest("5");
-                            _measuredValues2.RemoveAt(0);
                             result = Convert.ToDouble(gauge.GetMeasurement(Program.MeasurementType.BrMeasDb),
                                 CultureInfo.InvariantCulture);
+                            AutoscaleGraph((double)result, 5.0);
+                            _measuredValues2.RemoveAt(0);
                             _measuredValues2.Add(new(result));
-                            
                             line = $"{result.ToString()}\t 1500nm";
                             fw.WriteLine(line);
                         }
+
+                        progressBarIndex.Value++;
                     }
                 }
             }
         }
+        isStartEnabled.Value = true;
+        progressBarVisible.Value = Visibility.Hidden;
     }
 
     public void HaltExperiment()
